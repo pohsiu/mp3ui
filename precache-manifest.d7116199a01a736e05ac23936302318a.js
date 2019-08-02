677b32f27761e13635c0aa65c81c31ce061e2b25
Louis <louis.hsiao@d8ai.com>
2019-08-02 13:51:10 +0800

    Updates

---
+++ precache-manifest.d7116199a01a736e05ac23936302318a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a300e4ee4133d8429cd2304322a90155",
+    "url": "/mp3ui/index.html"
+  },
+  {
+    "revision": "7797f3363f409aedb143",
+    "url": "/mp3ui/static/css/main.7c0fda5d.chunk.css"
+  },
+  {
+    "revision": "55f1ba9697ebcd5c545d",
+    "url": "/mp3ui/static/js/2.b093485d.chunk.js"
+  },
+  {
+    "revision": "7797f3363f409aedb143",
+    "url": "/mp3ui/static/js/main.d21a42ea.chunk.js"
+  },
+  {
+    "revision": "86c652d3cdf492ef5e80",
+    "url": "/mp3ui/static/js/runtime~main.e6ef9712.js"
+  }
+]);
