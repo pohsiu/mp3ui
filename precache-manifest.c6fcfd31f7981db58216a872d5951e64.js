5967fa8c3baea1934fe92df51ffdca5311f1405c
Louis <louis.hsiao@d8ai.com>
2019-08-02 12:57:19 +0800

    Updates

---
+++ precache-manifest.c6fcfd31f7981db58216a872d5951e64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9befd133afe8403fc4c317c332fc1ddd",
+    "url": "/pohsiu/mp3ui/index.html"
+  },
+  {
+    "revision": "16c9b69404cbb4f36d4c",
+    "url": "/pohsiu/mp3ui/static/css/main.7c0fda5d.chunk.css"
+  },
+  {
+    "revision": "55f1ba9697ebcd5c545d",
+    "url": "/pohsiu/mp3ui/static/js/2.b093485d.chunk.js"
+  },
+  {
+    "revision": "16c9b69404cbb4f36d4c",
+    "url": "/pohsiu/mp3ui/static/js/main.60cddc86.chunk.js"
+  },
+  {
+    "revision": "0261c07a76fd90aa48a9",
+    "url": "/pohsiu/mp3ui/static/js/runtime~main.9161163d.js"
+  }
+]);
